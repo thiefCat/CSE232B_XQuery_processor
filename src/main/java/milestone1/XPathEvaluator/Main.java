8f02a2b7426e06ca206b501a93e8a009f408a256
Jinyi Wan <1w2j3y@sjtu.edu.cn>
2025-02-02 13:04:19 -0800

    XML process: Output result

--- src/main/java/milestone1/XPathEvaluator/Main.java
+++ src/main/java/milestone1/XPathEvaluator/Main.java
@@ -4,14 +4,24 @@ import java.util.List;
 
 import milestone1.autogen.ExpressionParser.XPathLexer;
 import milestone1.autogen.ExpressionParser.XPathParser;
+import javax.xml.parsers.ParserConfigurationException;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import javax.xml.transform.*;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
+import java.io.OutputStream;
 import org.w3c.dom.Node;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static TransformerFactory transformerFactory = TransformerFactory.newInstance();
+
     public static void main(String[] args) throws Exception {
         String fname = args[0];
         XPathLexer lexer = new XPathLexer(new ANTLRFileStream(fname));
@@ -38,21 +48,41 @@ public class Main {
      * Call visit functions on the tree to evaluate
      */
     public static List<Node> evaluate(String xPathQuery) {
+
         return null;
     }
 
-    /* TODO:
-     * Used to generate the final result in xml
+    /* Used to generate the final result in xml
      */
-    public static Document transform(List<Node> result) throws Exception{
-        return null;
+    public static Document transform(List<Node> result) throws ParserConfigurationException {
+        var dbFactory = DocumentBuilderFactory.newInstance();
+        var dBuilder = dbFactory.newDocumentBuilder();
+        var doc = dBuilder.newDocument();
+
+        var parentElement = doc.createElement("result");
+        doc.appendChild(parentElement);
+
+        for (Node node : result) {
+            try {
+                Node newNode;
+                newNode = doc.importNode(node, true);
+                parentElement.appendChild(newNode);
+            } catch (DOMException e) {
+                if (e.code != DOMException.NOT_SUPPORTED_ERR) {
+                    throw e;
+                }
+            }
+        }
+        return doc;
     }
 
-    /* TODO:
-     * Save the Document result to a xml file
-     */
-    public static void save_xml(Document result){
-        return;
+    /*
+    * Save the Document result to a xml file
+    */
+    public static void save_xml(Document result, OutputStream outputStream) throws TransformerException {
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+        transformer.transform(new DOMSource(result),new StreamResult(outputStream));
     }
-
 }
