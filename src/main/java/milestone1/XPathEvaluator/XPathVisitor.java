fede48e516ae2d5068b79a71714c4d57d34ee948
Jinyi Wan <1w2j3y@sjtu.edu.cn>
2025-02-02 10:20:10 -0800

    Add skeleton for XPathVisitor

--- src/main/java/milestone1/XPathEvaluator/XPathVisitor.java
+++ src/main/java/milestone1/XPathEvaluator/XPathVisitor.java
@@ -17,4 +17,92 @@ public class XPathVisitor extends XPathBaseVisitor<List<Node>>{
      *  implement visit for all fields after the hashtag'#' in XPath.g4
      */
 
+    // Begin: Absolute Path
+    // doc '/' relativePath     # childAP
+    @Override
+    public List<Node> visitChildAP(XPathParser.ChildAPContext ctx) {
+
+    }
+
+    // doc '//' relativePath    # descendantAP
+    @Override
+	public List<Node> visitDescendantAP(XPathParser.DescendantAPContext ctx) {
+
+    }
+    // End: Absolute Path
+
+    // Begin: RelativePath
+    //  tagName       # tagRP
+    @Override public List<Node> visitTagRP(XPathParser.TagRPContext ctx) {
+    }
+
+    // '*'           # allChildrenRP
+    @Override public List<Node>  visitAllChildrenRP(XPathParser.AllChildrenRPContext ctx) {
+
+    }
+
+    // '.'           # selfRP
+    @Override public List<Node> visitSelfRP(XPathParser.SelfRPContext ctx) {
+    }
+
+    // '..'          # parentRP
+    @Override public List<Node>  visitParentRP(XPathParser.ParentRPContext ctx) {
+    }
+
+    // 'text()'      # textRP
+    @Override public List<Node> visitTextRP(XPathParser.TextRPContext ctx) {
+    }
+
+    // '@' attrName  # attributeRP
+    @Override public List<Node> visitAttributeRP(XPathParser.AttributeRPContext ctx) {
+    }
+
+    // '(' relativePath  ')'    # bracketRP
+    @Override public List<Node> visitBracketRP(XPathParser.BracketRPContext ctx) {
+    }
+
+    // relativePath  '/' relativePath      # childrenRP
+    @Override public List<Node> visitChildrenRP(XPathParser.ChildrenRPContext ctx) {
+    }
+
+    // relativePath  '//' relativePath     # descendantRP
+    @Override public List<Node> visitDescendantRP(XPathParser.DescendantRPContext ctx) {
+    }
+    // relativePath  '[' f ']'  # filterRP
+    @Override public List<Node> visitFilterRP(XPathParser.FilterRPContext ctx) {
+    }
+
+    // relativePath  ',' relativePath      # commaRP
+    @Override public List<Node> visitCommaRP(XPathParser.CommaRPContext ctx) {
+    }
+    // End: RelativePath
+
+    // Begin: Filter
+    // f   : relativePath         # rpFilter
+    @Override public List<Node>  visitRpFilter(XPathParser.RpFilterContext ctx) {
+    }
+    // | relativePath  EQ relativePath   # eqFilter
+    @Override public List<Node> visitEqFilter(XPathParser.EqFilterContext ctx) {
+    }
+
+    // | relativePath  IS relativePath   # isFilter
+    @Override public List<Node> visitIsFilter(XPathParser.IsFilterContext ctx) {
+    }
+    // TODO: | relativePath  '=' STRING # stringFilter
+    @Override public List<Node> visitStringFilter(XPathParser.StringFilterContext ctx) {
+    }
+    //| '(' f ')' # bracketFilter
+    @Override public List<Node>  visitBracketFilter(XPathParser.BracketFilterContext ctx) {
+    }
+    // | f 'and' f # andFilter
+    @Override public List<Node>  visitAndFilter(XPathParser.AndFilterContext ctx) {
+    }
+    // | f 'or' f  # orFilter
+    @Override public List<Node>  visitOrFilter(XPathParser.OrFilterContext ctx) {
+    }
+    // | 'not' f   # notFilter
+    @Override public List<Node>  visitNotFilter(XPathParser.NotFilterContext ctx) {
+    }
+    // End: Filter
+
 }
